a99633f55553fc8a250d6894c0bc95bc76565c85
carla-ng <carlitashata@gmail.com>
2022-03-22 22:43:08 +0100

    Entrylist and Entry components.

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="entry-container pointer p-3">
+
+      <div class="entry-title d-flex">
+          <span class="text-success fs-5 fw-bold">15</span>
+          <span class="mx-1 fs-5">July</span>
+          <span class="mx-1 fs-5 fw-light">2022, Thursday</span>
+      </div>
+
+      <div class="entry-description">
+            Nostrud ex veniam anim occaecat sint amet irure cupidatat enim cupidatat. Laboris magna irure nostrud duis elit eiusmod aute proident id. Tempor ullamco velit exercitation quis labore amet mollit irure nulla magna. Ipsum consectetur in quis id labore ea. Aliquip consequat quis proident consectetur occaecat officia do do mollit duis ad laborum. Aute deserunt minim est duis pariatur elit dolor fugiat ea enim incididunt ullamco.
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.entry-container {
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+
+    &:hover {
+        background-color: lighten($color: grey, $amount: 45);
+        transition: 0.2s all ease-in;
+    }
+
+    .entry-description {
+        font-size: 12px;
+    }
+}
+</style>
--- src/modules/daybook/components/Entrylist.vue
+++ src/modules/daybook/components/Entrylist.vue
@@ -1,9 +1,47 @@
 <template>
-  <h1>Entrylist!</h1>
+  <div class="entry-list-container">
+    <div class="px-2 pt-2">
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Search entry"
+      />
+    </div>
+
+    <div class="entry-scrollarea">
+      <Entry
+        v-for="item in 100" :key="item"
+      />
+    </div>
+
+  </div>
 </template>
 
 <script>
-export default {
+import { defineAsyncComponent } from 'vue'
 
+export default {
+  components: {
+    Entry: defineAsyncComponent( () => import('../components/Entry.vue') )
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+
+.entry-list-container {
+  border-right: 1px solid #2c3a50;
+  height: calc( 100vh - 56px);
+
+  input {
+    height: 40px;
+  }
+
+  .entry-scrollarea {
+    height: calc( 100vh - 110px );
+    margin-top: 10px;
+    overflow: scroll;
+  }
+}
+
+</style>
