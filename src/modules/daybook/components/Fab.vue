6a256a367233fc83f7a5883dc1faec256416634d
carla-ng <carlitashata@gmail.com>
2022-03-22 22:58:41 +0100

    NoEntrySelected and Fab  components.

---
+++ src/modules/daybook/components/Fab.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="btn btn-primary">
+      <i class="fa fa-2x fa-plus"></i>
+  </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+    border-radius: 100%;
+    
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+
+    height: 80px;
+    width: 80px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,7 +16,7 @@
 export default {
   methods: {
     goToDaybook() {
-      this.$router.push({ name: 'daybook' })
+      this.$router.push({ name: 'no-entry' })
     }  
   }
 }
