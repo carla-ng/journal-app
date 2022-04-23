4f6aa6e5435cc348d0f255e6f935e18002586c44
carla-ng <carlitashata@gmail.com>
2022-04-23 20:54:44 +0200

    Jest testing added.

---
+++ tests/unit/example.spec.js
@@ -0,0 +1,15 @@
+import { shallowMount } from '@vue/test-utils'
+
+describe('HelloWorld.vue', () => {
+  it('renders props.msg when passed', () => {
+
+    /*
+    const msg = 'new message'
+    const wrapper = shallowMount(HelloWorld, {
+      props: { msg }
+    })
+    expect(wrapper.text()).toMatch(msg)
+    */
+   
+  })
+})
