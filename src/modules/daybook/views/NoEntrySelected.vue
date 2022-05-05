4a2d737220eaf24b6b28defc3e8234c104dcf831
carla-ng <carlitashata@gmail.com>
2022-05-05 22:45:55 +0200

    Daybook router module tests.

--- src/modules/daybook/views/NoEntrySelected.vue
+++ src/modules/daybook/views/NoEntrySelected.vue
@@ -12,6 +12,7 @@
 import { defineAsyncComponent } from 'vue'
 
 export default {
+  name: 'NoEntrySelected',
   components: {
     Fab: defineAsyncComponent( () => import('../components/Fab.vue') )
   },
