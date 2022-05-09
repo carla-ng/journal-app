5a6c7933fc8c0a8829dc297ee0b59901002cc94a
carla-ng <carlitashata@gmail.com>
2022-05-09 21:04:08 +0200

    Journal module store tests.

---
+++ tests/unit/mock-data/test-journal-state.js
@@ -0,0 +1,17 @@
+
+export const journalState = {
+    isLoading: false,
+    entries: [
+        {
+            id: "ABC123",
+            date: "Sat Jul 18 2021",
+            text: "Hi from mock data"
+        },
+        {
+            id: "XYZ456",
+            date: "Sat Jul 16 2021",
+            picture: "https://res.cloudinary.com/dkfdkhhsd/image/upload/v1650664017/fxectjbnv5k5yc0v5eq6.jpg",
+            text: "Second example text"
+        }
+    ]
+}
---
+++ tests/unit/modules/daybook/store/journal/journal-module.spec.js
@@ -0,0 +1,30 @@
+import { createStore } from 'vuex'
+import journal from '@/modules/daybook/store/journal'
+import { journalState } from '../../../../mock-data/test-journal-state'
+
+
+const createVuexStore = ( initialState ) => createStore({
+    modules: {
+        journal: {
+            ...journal,
+            state: { ...initialState }
+        }
+    }
+})
+
+
+
+describe('Vuex - Journal Module tests', () => {
+
+    // Basic tests
+    test('Initial state', () => {
+
+        const store = createVuexStore( journalState )
+
+        const { isLoading, entries } = store.state.journal
+        expect( isLoading ).toBeFalsy()
+        expect( entries ).toEqual( journalState.entries )
+
+    })
+
+})
